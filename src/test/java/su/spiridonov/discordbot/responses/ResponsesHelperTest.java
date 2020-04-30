5f379c330d412b0009588b941cf73544ecee9fe1
Aleksey Spiridonov <aleksey@spiridonov.su>
2020-04-30 18:56:35 +0400

    Added test cases for !ping and unknown commands

---
+++ src/test/java/su/spiridonov/discordbot/responses/ResponsesHelperTest.java
@@ -0,0 +1,40 @@
+package su.spiridonov.discordbot.responses;
+
+import discord4j.core.ServiceMediator;
+import discord4j.core.object.data.stored.MessageBean;
+import discord4j.core.object.data.stored.UserBean;
+import discord4j.core.object.entity.Message;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ResponsesHelperTest {
+    String KNOWLEDGE_BASE = "src/main/resources/base.properties";
+    String USERNAME = "test";
+
+    @Test
+    public void validateNullResponse() {
+        Message message = createDiscordTestMessage("zzz");
+        ResponsesHelper responsesHelper = new ResponsesHelper(KNOWLEDGE_BASE);
+        assertEquals(null, responsesHelper.returnResponse(message));
+    }
+
+    @Test
+    public void validatePingResponse() {
+        Message message = createDiscordTestMessage("!ping");
+        ResponsesHelper responsesHelper = new ResponsesHelper(KNOWLEDGE_BASE);
+        assertEquals(USERNAME+" pong", responsesHelper.returnResponse(message));
+    }
+
+    private Message createDiscordTestMessage(String msg) {
+        ResponsesHelper responsesHelper = new ResponsesHelper(KNOWLEDGE_BASE);
+        UserBean userBean = new UserBean();
+        userBean.setUsername(USERNAME);
+        MessageBean testMessageBean = new MessageBean();
+        ServiceMediator nullMediator = new ServiceMediator(null, null, null, null, null, null, null);
+        Message testMsg = new Message(nullMediator, testMessageBean);
+        testMessageBean.setContent(msg);
+        testMessageBean.setAuthor(userBean);
+        return testMsg;
+    }
+}
